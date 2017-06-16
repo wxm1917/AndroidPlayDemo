74e0a20dfce0844c85651d8df62b24937601cf23
renamewang <renamewang@163.com>
2017-06-16 21:05:48 +0800

    1.AR视频Activity添加back键监听 2.解决AR视频Activity按back键前一个Activity异常退出

--- app/src/main/java/com/qiyi/openapi/demo/activity/UnityPlayerActivity.java
+++ app/src/main/java/com/qiyi/openapi/demo/activity/UnityPlayerActivity.java
@@ -1,17 +1,47 @@
 package com.qiyi.openapi.demo.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.Window;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
+import com.qiyi.openapi.demo.R;
 import com.unity3d.player.UnityPlayer;
 
 public class UnityPlayerActivity extends Activity {
     protected UnityPlayer mUnityPlayer; // don't change the name of this variable; referenced from native code
+    private RelativeLayout mCloseLayout;
+    private ImageView mCloseImageView;
+
+    /**
+     * 初始化视图
+     */
+    private void initView(){
+        // 获取显示unity视图的父控件
+        RelativeLayout parent = (RelativeLayout) findViewById(R.id.id_layout_unity);
+        // 获取unity视图
+        mUnityPlayer = new UnityPlayer(this);
+        View view = mUnityPlayer.getView();
+        // 将unity视图添加到Android视图中
+        parent.addView(view);
+
+        mCloseLayout = (RelativeLayout) findViewById(R.id.id_btn_close);
+        mCloseLayout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+//                finish();
+            }
+        });
+
+        mCloseImageView = (ImageView) findViewById(R.id.id_iv_close);
+    }
 
     // Setup activity layout
     @Override
@@ -19,11 +49,15 @@ public class UnityPlayerActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
 
-        getWindow().setFormat(PixelFormat.RGBX_8888); // <--- This makes xperia play happy
+//        getWindow().setFormat(PixelFormat.RGBX_8888); // <--- This makes xperia play happy
+//
+//        mUnityPlayer = new UnityPlayer(this);
+//        setContentView(mUnityPlayer);
+//        mUnityPlayer.requestFocus();
 
-        mUnityPlayer = new UnityPlayer(this);
-        setContentView(mUnityPlayer);
-        mUnityPlayer.requestFocus();
+        setContentView(R.layout.activity_unity_player);
+        getWindow().setFormat(PixelFormat.RGBX_8888);
+        initView();
     }
 
     // Quit Unity
@@ -45,6 +79,7 @@ public class UnityPlayerActivity extends Activity {
     protected void onResume() {
         super.onResume();
         mUnityPlayer.resume();
+//        mCloseImageView.setVisibility(View.VISIBLE);
     }
 
     // This ensures the layout will be correct.
@@ -78,7 +113,13 @@ public class UnityPlayerActivity extends Activity {
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK)) {
+            mUnityPlayer.quit();
+            finish();
+            return true;
+        }
         return mUnityPlayer.injectEvent(event);
+//        return mUnityPlayer.injectEvent(event);
     }
 
     @Override
